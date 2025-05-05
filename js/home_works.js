43d38b743531bcfbdfa60490b252f7ecb6bddd93
Bsoulsss <akbermetokelova0@gmail.com>
2025-05-05 16:13:57 +0600

    мой первый коммит

--- js/home_works.js
+++ js/home_works.js
@@ -1,20 +1,37 @@
-// MOVE BLOCK HW 1.2
+// дз1
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.form_gmail');
+  const input = form.querySelector('input[type="email"]');
+  const button = form.querySelector('button');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = input.value.trim();
+    const gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+
+    if (gmailRegex.test(email)) {
+      alert('Почта валидна!');
+    } else {
+      alert('Введите корректную Gmail почту (например: example@gmail.com)');
+    }
+  });
+});
+
 
 const parentBlock = document.querySelector('.parent_block');
 const childBlock = document.querySelector('.child_block');
 
-let positionX = 0
-let positionY = 0
+let positionX = 0;
+const maxX = parentBlock.clientWidth - childBlock.clientWidth;
 
-const offWidth = parentBlock.offsetWidth - childBlock.offsetWidth
-const offHeight = parentBlock.offsetHeight - childBlock.offsetHeight
+function moveRight() {
+  childBlock.style.left = `${positionX}px`;
 
-const moveBlock = () => {
-    if (positionX < offWidth) positionX++
-    if (positionX >= offWidth && positionY < offHeight) positionY++
-    childBlock.style.left = `${positionX}px`
-    childBlock.style.top = `${positionY}px`
-    requestAnimationFrame(moveBlock)
+  if (positionX < maxX) {
+    positionX++;
+    requestAnimationFrame(moveRight);
+  }
 }
 
-moveBlock();
+moveRight();
+
